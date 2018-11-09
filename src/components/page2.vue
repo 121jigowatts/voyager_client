5590f88795cc31f1d1e7a42428f380fdc8d14372
soil <sh.yoshida@live.jp>
2018-11-10 06:00:19 +0900

    add axios

--- src/components/page2.vue
+++ src/components/page2.vue
@@ -9,10 +9,26 @@
     <p>
       <router-link to="/">Go to page1</router-link>
     </p>
+    <div>
+      {{ info }}
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      info: null
+    };
+  },
+  mounted() {
+    axios
+      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then(response => (this.info = response));
+  }
+}
 </script>
 
 <style>
