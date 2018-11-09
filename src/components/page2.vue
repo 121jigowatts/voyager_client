e5b0c2c924836e1b0712961ec1919786931d03f1
soil <sh.yoshida@live.jp>
2018-11-10 06:25:37 +0900

    Display data to get from the API

--- src/components/page2.vue
+++ src/components/page2.vue
@@ -9,14 +9,20 @@
     <p>
       <router-link to="/">Go to page1</router-link>
     </p>
-    <div>
-      {{ info }}
+    <h1>Bitcoin Price Index</h1>
+    <div v-for="currency in info" :key="currency.code"
+    class="currency">
+      {{currency.description}}
+      <span class="lighten">
+        <span v-html="currency.symbol"></span>
+        {{ currency.rate_float | currencydecimal }}
+      </span>
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
   data() {
     return {
@@ -26,9 +32,14 @@ export default {
   mounted() {
     axios
       .get("https://api.coindesk.com/v1/bpi/currentprice.json")
-      .then(response => (this.info = response));
+      .then(response => (this.info = response.data.bpi));
+  },
+  filters: {
+    currencydecimal(value) {
+      return value.toFixed(2);
+    }
   }
-}
+};
 </script>
 
 <style>
